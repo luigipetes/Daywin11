aa6738b92f44e8be4b82ed824f149ca7498bd771
Luke <luke.saffy@outlook.com>
2024-05-19 21:33:59 +0200

    Initial Commit

---
+++ DayWin/DayWinApp.swift
@@ -0,0 +1,32 @@
+//
+//  DayWinApp.swift
+//  DayWin
+//
+//  Created by Luke Saffy on 2024/05/19.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DayWinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
